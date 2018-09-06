01138d1b29a5902f5985f0f3cd1807510092287f
Daniel <gwydionp17@gmail.com>
2018-09-06 04:00:10 -0300

    Fix of previous commit (:

--- src/main/java/net/gwyddie/logparser/Program.java
+++ src/main/java/net/gwyddie/logparser/Program.java
@@ -1,4 +1,13 @@
 package net.gwyddie.logparser;
 
+import java.util.List;
+
 public class Program {
+    public static void main(String[] args) throws Exception {
+        List<String> lines = new Reader("games.log").read();
+
+        for (int i = 0; i < lines.size(); i++) {
+            System.out.println(lines.get(i));
+        }
+    }
 }
--- src/main/java/net/gwyddie/logparser/Reader.java
+++ src/main/java/net/gwyddie/logparser/Reader.java
@@ -1,10 +1,39 @@
 package net.gwyddie.logparser;
 
-public class File {
-    private String path;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-    public File(String path) {
+public class Reader {
+    private File file;
 
-        this.path = path;
+    public Reader(String path) {
+        this.file = new File(path);
+    }
+
+    public boolean isFile() {
+        return file.exists() && file.isFile();
+    }
+
+    public boolean isReachable() {
+        return isFile() && file.canRead();
+    }
+
+    public List<String> read() {
+        List<String> lines = new ArrayList<>();
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return lines;
     }
 }
