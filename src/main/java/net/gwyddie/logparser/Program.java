b78bbbede2a3da09f608a3dc0f80c2d1d8b887fa
Daniel <gwydionp17@gmail.com>
2018-09-06 03:44:00 -0300

    File handling added (open and read)

---
+++ src/main/java/net/gwyddie/logparser/Program.java
@@ -0,0 +1,4 @@
+package net.gwyddie.logparser;
+
+public class Program {
+}
---
+++ src/main/java/net/gwyddie/logparser/Reader.java
@@ -0,0 +1,10 @@
+package net.gwyddie.logparser;
+
+public class File {
+    private String path;
+
+    public File(String path) {
+
+        this.path = path;
+    }
+}
